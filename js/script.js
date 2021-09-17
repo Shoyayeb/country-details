c80f0446b69ad65f5b8ee3c6f7edfe9cc4d1d028
Shoyayeb <sa.fine@Outlook.com>
2021-09-17 20:07:24 +0600

    all country showing with grid

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const showCountry = (countries) => {
     console.log(countries[0]);
     const countryHTML = countries.map(country => getCountyHTML(country));
     const container = document.getElementById("countries")
-    container.innerHTML = countryHTML[0]
+    container.innerHTML = countryHTML.join('')
 }
 const getCountyHTML = country => {
     return `
-        <div>
+        <div class="country">
             <h2>${country.name}</h2>
             <h4> Capital: ${country.capital}</h4>
             <img src="${country.flag}">
